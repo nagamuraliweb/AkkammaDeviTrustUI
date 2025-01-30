c6d851790bbabcd79c2e865c1d500ba9fd477e09
Nagamurali <nagamuraliweb@gmail.com>
2025-01-30 18:58:03 +0530

    Login react

--- GaliyarKulaWelfareTrust-UI/src/pages/login/login.js
+++ GaliyarKulaWelfareTrust-UI/src/pages/login/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
@@ -11,9 +11,23 @@ import './login.css';
 
 function Login() {
     const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [usernameError, setUsernameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
 
     const handleLogin = () => {
-        navigate('/donation-list');
+        if (!username) {
+            setUsernameError(true);
+        }
+        if (!password) {
+            setPasswordError(true);
+        }
+
+        if (username && password) {
+
+        }
+        //navigate('/donation-list');
     }
 
     return (
@@ -25,12 +39,14 @@ function Login() {
                         <Form>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>USERNAME</Form.Label>
-                                <Form.Control type="text" placeholder="Enter username" />
+                                <Form.Control type="text" placeholder="Enter username" name="username" onChange={(e) => setUsername(e.target.value)} />
+                                {usernameError ? <span className="filed-error">Please enter username</span> : <></>}
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                 <Form.Label>PASSWORD</Form.Label>
-                                <Form.Control type="password" placeholder="Enter password" />
+                                <Form.Control type="password" placeholder="Enter password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                                {passwordError ? <span className="filed-error">Please enter password</span> : <></>}
                             </Form.Group>
                             <Button variant="primary" className='button-style' onClick={handleLogin}>
                                 Sign in
