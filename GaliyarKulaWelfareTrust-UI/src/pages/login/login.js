56d0a1eed2f0bcaec67cd7b4cbf82e9b411a6d3f
Nagamurali <nagamuraliweb@gmail.com>
2025-01-30 19:48:42 +0530

    Login validation

--- GaliyarKulaWelfareTrust-UI/src/pages/login/login.js
+++ GaliyarKulaWelfareTrust-UI/src/pages/login/login.js
@@ -15,6 +15,24 @@ function Login() {
     const [usernameError, setUsernameError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
 
+    const handleUsername = (name) => {
+        setUsername(name);
+        if (name) {
+            setUsernameError(false);
+        } else {
+            setUsernameError(true);
+        }
+    }
+
+    const handlePassword = (pwd) => {
+        setPassword(pwd);
+        if (pwd) {
+            setPasswordError(false);
+        } else {
+            setPasswordError(true);
+        }
+    }
+
     const handleLogin = async () => {
         if (!username) {
             setUsernameError(true);
@@ -42,16 +60,16 @@ function Login() {
                         <Form>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>USERNAME</Form.Label>
-                                <Form.Control type="text" placeholder="Enter username" name="username" onChange={(e) => setUsername(e.target.value)} />
-                                {usernameError ? <span className="filed-error">Please enter username</span> : <></>}
+                                <Form.Control type="text" placeholder="Enter username" name="username" onChange={(e) => handleUsername(e.target.value)} />
+                                {usernameError ? <span className="field-error">Please enter username</span> : <></>}
                             </Form.Group >
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Group className="mb-4" controlId="formBasicPassword">
                                 <Form.Label>PASSWORD</Form.Label>
-                                <Form.Control type="password" placeholder="Enter password" name="password" onChange={(e) => setPassword(e.target.value)} />
-                                {passwordError ? <span className="filed-error">Please enter password</span> : <></>}
+                                <Form.Control type="password" placeholder="Enter password" name="password" onChange={(e) => handlePassword(e.target.value)} />
+                                {passwordError ? <span className="field-error">Please enter password</span> : <></>}
                             </Form.Group >
-                            <Button disabled={username === '' || password === ''} variant="primary" className='button-style' onClick={handleLogin}>
+                            <Button variant="primary" className='button-style' onClick={handleLogin}>
                                 Sign in
                             </Button>
                         </Form >
